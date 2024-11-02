91c87412ea548da84fe7aa47bad9eccf8844f13f
Rafael Pastor <rafa.f.pastor@gmail.com>
2024-11-02 07:53:29 -0300

    WIP(Added initial files)

---
+++ questionOne/src/main/java/com/exemplo/star_wars_planetas/StarWarsPlanetasApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.star_wars_planetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetasApplication.class, args);
+	}
+
+}
---
+++ questionOne/src/test/java/com/exemplo/star_wars_planetas/StarWarsPlanetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.star_wars_planetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
